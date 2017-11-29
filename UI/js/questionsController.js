433c914bbd2b94fab7abfda69d798180713e0f97
Samarth <sam3ks@gmail.com>
2017-11-29 11:16:54 +0530

    Moving angular related code with mock reponse

--- UI/js/questionsController.js
+++ UI/js/questionsController.js
@@ -1,120 +1,72 @@
 (function () {
     angular.module('quizModule')
-        .controller('quizController', function ($scope) {
+        .controller('quizController', function ($scope, $http) {
 
-            $scope.currentQuestion = {};          
+            $scope.currentQuestion = {};
             $scope.totalNumberOfQuestions = 0;
             $scope.numberOfQuestionsAnswered = 0;
             $scope.questionIndex = -1;
             $scope.quizStarted = false;
-            $scope.completionPecentage=0;
-            $scope.quizCompleted =false;
-            $scope.submitQuiz =submitQuiz;
+            $scope.completionPecentage = 0;
+            $scope.quizCompleted = false;
+            $scope.submitQuiz = submitQuiz;
             $scope.setCurrentQuestion = setCurrentQuestion;
-            function reset(){
-                $scope.currentQuestion = {};          
+            function reset() {
+                $scope.currentQuestion = {};
                 $scope.totalNumberOfQuestions = 0;
                 $scope.numberOfQuestionsAnswered = 0;
                 $scope.questionIndex = -1;
                 $scope.quizStarted = false;
-                $scope.completionPecentage=0;
-                $scope.quizCompleted =false;
+                $scope.completionPecentage = 0;
+                $scope.quizCompleted = false;
             }
             function setCurrentQuestion() {
                 updateQuizProgress();
-                $scope.currentQuestion =  $scope.questionsList[ $scope.questionIndex];
+                $scope.currentQuestion = $scope.questionsList[$scope.questionIndex];
             }
             function updateQuizProgress() {
-                $scope.questionIndex++;             
-                $scope.completionPecentage =($scope.numberOfQuestionsAnswered/$scope.totalNumberOfQuestions)*100;
+                $scope.questionIndex++;
+                $scope.completionPecentage = ($scope.numberOfQuestionsAnswered / $scope.totalNumberOfQuestions) * 100;
             }
 
-            $scope.questionsList = [
-                {
-                    questionId: 123,
-                    questionDesc: "For which of the following disciplines is Nobel Prize awarded?",
-                    options: [
-                        {
-                            optionId: "A",
-                            optionDesc: "Physics and Chemistry"
-                        },
-                        {
-                            optionId: "B",
-                            optionDesc: "Physiology or Medicine"
-                        },
-                        {
-                            optionId: "C",
-                            optionDesc: "Literature, Peace and Economics"
-                        },
-                        {
-                            optionId: "D",
-                            optionDesc: "All of the above"
-                        }]
-                },
-                {
-                    questionId: 124,
-                    questionDesc: "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
-                    options: [
-                        {
-                            optionId: "A",
-                            optionDesc: "Asia"
-                        },
-                        {
-                            optionId: "B",
-                            optionDesc: "Africa"
-                        },
-                        {
-                            optionId: "C",
-                            optionDesc: "Europe"
-                        },
-                        {
-                            optionId: "D",
-                            optionDesc: "Australia"
-                        }]
-                },                
-                {
-                    questionId: 125,
-                    questionDesc: "The world’s first hybrid electric tram powered by hydrogen fuel cells has started in which country?",
-                    options: [
-                        {
-                            optionId: "A",
-                            optionDesc: "Germany"
-                        },
-                        {
-                            optionId: "B",
-                            optionDesc: "China"
-                        },
-                        {
-                            optionId: "C",
-                            optionDesc: "France"
-                        },
-                        {
-                            optionId: "D",
-                            optionDesc: "Australia"
-                        }]
-                }];
+            $scope.questionsList = [];
+            $scope.fetchQuestions = fetchQuestions;
             $scope.startQuiz = startQuiz;
-
+            function fetchQuestions() {
+                var API_URL = 'http://localhost:3000/questions';
+                $http.get(API_URL).then(function (response) {
+                    console.log(response);
+                    $scope.questionsList = response.data;
+                    console.log($scope.questionsList);
+                    $scope.totalNumberOfQuestions = $scope.questionsList.length;
+                    $scope.setCurrentQuestion();
+                    $scope.quizStarted = true;
+                },
+                    function (error) {
+                        console.log("Error");
+                    }
+                );
+            }
             function startQuiz() {
-                $scope.totalNumberOfQuestions =  $scope.questionsList.length;
-                $scope.setCurrentQuestion();
-                $scope.quizStarted = true;
+                reset();
+                $scope.fetchQuestions();
+
             }
             $scope.submitAnswer = submitAnswer;
             function submitAnswer() {
                 $scope.numberOfQuestionsAnswered++;
                 if ($scope.numberOfQuestionsAnswered >= $scope.totalNumberOfQuestions) {
-                    if ($scope.numberOfQuestionsAnswered == $scope.totalNumberOfQuestions){                      
+                    if ($scope.numberOfQuestionsAnswered == $scope.totalNumberOfQuestions) {
                         $scope.quizCompleted = true;
-                         updateQuizProgress();
+                        updateQuizProgress();
                     }
-                }else {
+                } else {
                     $scope.setCurrentQuestion();
                 }
             }
-            function submitQuiz(){
-                  $scope.quizStarted = false;
-                  reset();
+            function submitQuiz() {
+                $scope.quizStarted = false;
+                reset();
 
             }
         });
