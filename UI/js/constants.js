4af719923691180ee4c34c29c28f34ec8a739320
Samarth <sam3ks@gmail.com>
2017-12-03 18:26:48 +0530

    updated questions endpoint to https

--- UI/js/constants.js
+++ UI/js/constants.js
@@ -1,7 +1,7 @@
 (function(){
 angular.module('quizModule')
        .constant('endPoints',{
-    "FETCH_QUESTIONS_LIST":"http://ec2-13-127-55-121.ap-south-1.compute.amazonaws.com:3000/questions"
+    "FETCH_QUESTIONS_LIST":"https://ec2-13-127-55-121.ap-south-1.compute.amazonaws.com:3000/questions"
 })
 })();
 
